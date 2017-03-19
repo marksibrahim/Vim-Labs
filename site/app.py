dea7553810e3d8071499869a11e8d8755fc3e699
marksibrahim <mark.ibhm@gmail.com>
2017-03-19 14:52:04 -0400

    autoreload and module 3 steps added

--- site/app.py
+++ site/app.py
@@ -5,6 +5,8 @@ runs flask app
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
 @app.route("/")
